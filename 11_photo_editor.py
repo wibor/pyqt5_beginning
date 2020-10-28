88a6a94f1bca786e8a21c5e8d0ffc953da2b13c8
vb0478 <vb047851@student.ath.edu.pl>
2020-10-28 23:55:26 +0100

    Edit 11 photoEditor
    in progress, unfinished

--- 11_photo_editor.py
+++ 11_photo_editor.py
@@ -1,8 +1,9 @@
 import sys
 
-from PyQt5.QtCore import QSize
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit, QAction, QStatusBar, QToolBar
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit, QAction, QStatusBar, QToolBar, QDockWidget, \
+    QDesktopWidget, QWidget, QPushButton, QVBoxLayout, QLabel, QSizePolicy
 
 
 class PhotoEditor(QMainWindow):
@@ -11,36 +12,173 @@ class PhotoEditor(QMainWindow):
         self.Gui()
 
     def Gui(self):
-        self.setGeometry(100, 100, 400, 350)
+        #self.setGeometry(100, 100, 650, 650)
+        self.setFixedSize(650, 650)
         self.setWindowTitle('Photo Editor')
-        self.setCentralWidget(QTextEdit())
+        #self.setCentralWidget(QTextEdit())
+        self.centerMainWindow()
 
+        self.toggle_dock_tools_act = None
         self.createMenu()
         self.createToolBar()
-        #self.createDockWidget()
+        self.createToolsDockWidget()
+        self.photoEditorWidgets()
 
         self.show()
 
+    def centerMainWindow(self):
+        desktop = QDesktopWidget().screenGeometry()
+        screen_width = desktop.width()
+        screen_height = desktop.height()
+        self.move(int((screen_width - self.width()) / 2), int((screen_height - self. height()) / 2))
+
     def createToolBar(self):
-        tool_bar = QToolBar("Main Toolbar")
-        tool_bar.setIconSize(QSize(16,16))
+        tool_bar = QToolBar("Toolbar")
+        # You should set the size of the icons in the toolbar using the setIconSize() method
+        # with QSize() to avoid extra padding
+        # when PyQt tries to figure out the arrangement by itself.
+        tool_bar.setIconSize(QSize(24,24))
         self.addToolBar(tool_bar)
 
+        tool_bar.addAction(self.open_act)
+        tool_bar.addAction(self.save_act)
+        tool_bar.addAction(self.print_act)
+        tool_bar.addSeparator()
         tool_bar.addAction(self.exit_act)
 
     def createMenu(self):
         # create actions
         self.exit_act = QAction(QIcon('images/exit.png'), 'Exit', self)
+        self.open_act = QAction(QIcon('images/open_file.png'), 'Open', self)
+        self.save_act = QAction(QIcon('images/save_file.png'), 'Save', self)
+        self.print_act = QAction(QIcon('images/print.png'), 'Print', self)
         self.exit_act.setShortcut('Ctrl+Q')
+        self.open_act.setShortcut('Ctrl+O')
+        self.save_act.setShortcut('Ctrl+S')
+        self.print_act.setShortcut('Ctrl+P')
+        # In order to display a message in the status bar when the mouse hovers over an icon,
         self.exit_act.setStatusTip('Quit program')
         self.exit_act.triggered.connect(self.close)
+        self.open_act.triggered.connect(self.openImage)
+        self.save_act.triggered.connect(self.saveImage)
+        self.print_act.triggered.connect(self.printImage)
+        self.print_act.setEnabled(False)
 
         menu_bar = self.menuBar()
         # menu_bar.setNativeMenuBar(False)
         file_menu = menu_bar.addMenu('File')
+
+        file_menu.addAction(self.open_act)
+        file_menu.addAction(self.save_act)
+        file_menu.addSeparator()
+        file_menu.addAction(self.print_act)
+        file_menu.addSeparator()
         file_menu.addAction(self.exit_act)
 
+        # Create actions for edit menu
+        self.rotate90_act = QAction("Rotate 90°", self)
+        self.flip_hor_act = QAction("Flip Horizonal°", self)
+        self.flip_ver_act = QAction("Flip Vertical°", self)
+        self.resize_act = QAction("Resize Image°", self)
+        self.rotate90_act.setShortcut("Ctrl+R")
+        self.flip_hor_act.setShortcut("Ctrl+H")
+        self.flip_ver_act.setShortcut("Ctrl+V")
+        self.rotate90_act.triggered.connect(self.rotateImage)
+        self.flip_hor_act.triggered.connect(self.flipHorImage)
+        self.flip_ver_act.triggered.connect(self.flipVerImage)
+        self.resize_act.triggered.connect(self.resizeImage)
+
+        # Create edit menu and add actions
+        edit_menu = menu_bar.addMenu('Edit')
+
         self.setStatusBar(QStatusBar(self))
+        edit_menu.addAction(self.rotate90_act)
+        edit_menu.addAction(self.flip_hor_act)
+        edit_menu.addAction(self.flip_ver_act)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.resize_act)
+
+        # Create view menu and add actions
+        view_menu = menu_bar.addMenu('View')
+        self.dock = QDockWidget()
+        self.toggle_dock_tools_act = self.dock.toggleViewAction()
+        view_menu.addAction(self.toggle_dock_tools_act)
+
+        self.setStatusBar(QStatusBar(self))
+
+    def createToolsDockWidget(self):
+        # Use View -> Edit Image Tools menu and click the dock widget on or off.
+        self.dock = QDockWidget()
+        self.dock.setWindowTitle('Dock')
+        #dock.setAllowedAreas(Qt.AllDockWidgetAreas)
+        self.dock.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
+
+        #dock.setWidget(QTextEdit())
+        # Create container QWidget to hold all widgets inside dock widget
+        self.toolsDock = QWidget()
+        # Set initial location of dock widget in main window (Left area)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dock)
+        # In order to place multiple widgets inside the dock,
+        # you could use a single QWidget as the parent for multiple child widgets
+        # and arrange them using one of the layout managers
+        # Create tool push buttons:
+        self.rotate90 = QPushButton("Rotate 90°")
+        self.rotate90.setMinimumSize(QSize(130, 40))
+        self.rotate90.clicked.connect(self.rotateImage)
+        self.flip_horizontal = QPushButton("Flip Horizontal")
+        self.flip_horizontal.setMinimumSize(QSize(130, 40))
+        self.flip_horizontal.clicked.connect(self.flipHorImage)
+        self.flip_vertical = QPushButton("Flip Vertical")
+        self.flip_vertical.setMinimumSize(QSize(130, 40))
+        self.flip_vertical.clicked.connect(self.flipVerImage)
+        self.resize_half = QPushButton("Resize Half")
+        self.resize_half.setMinimumSize(QSize(130, 40))
+        self.resize_half.clicked.connect(self.resizeImage)
+
+        # Set up vertical layout to contain all the push buttons
+        dock_vert_box = QVBoxLayout()
+        dock_vert_box.addWidget(self.rotate90)
+        dock_vert_box.addStretch(1)
+        dock_vert_box.addWidget(self.flip_horizontal)
+        dock_vert_box.addWidget(self.flip_vertical)
+        dock_vert_box.addStretch(1)
+        dock_vert_box.addWidget(self.resize_half)
+        dock_vert_box.addStretch(6)
+        # Set the main layout for the QWidget, tools_contents,
+        # then set the main widget of the dock widget
+        self.toolsDock.setLayout(dock_vert_box)
+        self.dock.setWidget(self.toolsDock)
+
+        # Handles the visibility of the dock widget
+        self.toggle_dock_tools_act = self.dock.toggleViewAction()
+
+    def photoEditorWidgets(self):
+        # Set up instances of widgets for photo editor GUI
+        self.image = QPixmap()
+        self.image_label = QLabel()
+        self.image_label.setAlignment(Qt.AlignCenter)
+        # Use setSizePolicy to specify how the widget can be resized,
+        # horizontally and vertically. Here, the image will stretch
+        # horizontally, but not vertically.
+        self.image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Ignored)
+
+        self.setCentralWidget(self.image_label)
+
+    def openImage(self):
+        pass
+    def saveImage(self):
+        pass
+    def printImage(self):
+        pass
+    def rotateImage(self):
+        pass
+    def flipHorImage(self):
+        pass
+    def flipVerImage(self):
+        pass
+    def resizeImage(self):
+        pass
+
 
 
 if __name__ == '__main__':
